79b42ae3908b9499375e9aae3fd256b97d8520bc
Jerin Philip <jerinphilip@live.in>
2023-07-06 13:41:22 +0530

    Add a note on the `const_cast`

--- fcl/types.cc
+++ fcl/types.cc
@@ -4,6 +4,13 @@
 #include <iostream>
 
 std::ostream &operator<<(std::ostream &out, const Point &point) {
+  // Compiler refuses to work with p.x, as it is int and not const int.
+  //
+  // So some ninja techniques (const_cast) are applied here to cast away the
+  // requirement.
+  //
+  // This is printing code, and point.x is not modified in the process, so
+  // should be okay.
   int *x = const_cast<int *>(&point.x);
   int *y = const_cast<int *>(&point.y);
   out << "(" << *x << " " << *y << ")";
